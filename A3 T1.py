b72ae9b804684d2988956425e0bc7e022c679b84
captainmaroof737-afk <captainmaroof737@gmail.com>
2025-09-16 09:19:37 +0530

    Add files via upload

---
+++ A3 T1.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):   # Loop from 1 to n
+        result *= i
+    return result
+num = 5
+print("Factorial of", num, "is:", factorial(num))
---
+++ A3 T2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine = math.sin(num)
+
+print("Square root of", num, "is:", square_root)
+print("logarithm of", num, "is:", logarithm)
+print("Sine of", num, "is:", sine)
